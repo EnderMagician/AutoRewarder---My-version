1a60d62e87cecf523eb974c7c7ca2f020007cbad
Nguyễn Minh Hiếu <hieunmnguyenminh@gmail.com>
2026-09-09 19:09:43 +0700

    chore(release): prepare v4.3.1

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ import os
 import platform
 import sys
 
-CURRENT_VERSION = "v4.3"
+CURRENT_VERSION = "v4.3.1"
 REPO = "EnderMagician/AutoRewarder---My-version"
 
 PLATFORM_NAME = platform.system()
